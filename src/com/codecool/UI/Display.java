58a34beeba0a51762945cc7ea83294a1842ec66c
damianTrybuszek <damian.trybuszek@gmail.com>
2021-08-29 17:18:00 +0200

    Main menu improved, input improved. Game logic started to make.

--- src/com/codecool/UI/Display.java
+++ src/com/codecool/UI/Display.java
@@ -1,36 +1,53 @@
 package com.codecool.UI;
 
 public class Display {
+    Input input = new Input();
 
-    public void printGameMenu(){
+    public String[] printGameMenu() {
         System.out.println("Hello Dear User! Welcome to Battleships Game!");
         System.out.println("Select Game mode, please!");
         System.out.println("If you want to choose mode player vs player please select 1.");
         System.out.println("If you want to choose mode player vs computer, please select 2.");
         System.out.println("If you want to choose mode computer vs computer, please select 3.");
-        Input input = new Input();
-        int gameMode = input.sc.nextInt();
-        if (gameMode == 1) {
-            System.out.println("You chose Player vs Player mode, please insert the name of the first player:");
-            String playerName1 = input.sc.nextLine();
-            System.out.println("Please input the second Player name:");
-            String playerName2 = input.sc.nextLine();
+        String gameMode = input.getGameMode();
+
+        switch (gameMode) {
+            case "1": {
+                System.out.println("You chose Player vs Player mode, please insert the name of the first player:");
+                String playerName1 = input.getPlayerName();
+                System.out.println("Please input the second Player name:");
+                String playerName2 = input.getPlayerName();
+                return new String[]{gameMode, playerName1, playerName2};
+            }
+
+            case "2": {
+                System.out.println("You chose Player vs computer mode, please insert the name of the player:");
+                String playerName = input.sc.nextLine();
+                System.out.println("Please select the difficulty level within a range of 1 to 3, 1 is easiest:");
+                String difficultyLevel = input.getGameMode();
+                return new String[]{gameMode, playerName, difficultyLevel};
+            }
+            case "3": {
+                System.out.println("You chose the computer vs computer game mode. Just sit back and enjoy the battle.");
+                return new String[]{gameMode};
+            }
+
         }
-        else if (gameMode == 2) {
-            System.out.println("You chose Player vs computer mode, please insert the name of the player:");
-            String playerName = input.sc.nextLine();
-            System.out.println("Please select the difficulty level within a range of 1 to 3, 1 is easiest:");
-            int difficultyLevel = input.sc.nextInt();
-        }
-        else {
-            System.out.println("You chose the computer vs computer game mode. Just sit back and enjoy the battle.");
-        }
-    };
+        return new String[] {"0"};
+    }
+
+
     public void printBoard(){};
     public void printGameplay(){};
     public void printOutcome(){};
+    public void printWrongValueAlert(String valueRange){
+        System.out.println("Please provide correct value in " + valueRange + ".");
+    }
+
+    protected void printWrongNameAlert(){
+                System.out.println("Please provide longer planer name.");
+    }
+        };
 
 
 
-
-}
--- src/com/codecool/UI/Input.java
+++ src/com/codecool/UI/Input.java
@@ -1,14 +1,43 @@
 package com.codecool.UI;
 
+import java.util.ArrayList;
 import java.util.Scanner;
+import com.codecool.UI.Display;
+
+import static java.lang.Integer.parseInt;
 
 public class Input {
     Scanner sc = new Scanner(System.in);
+//    Display display = new Display();
+
 
     public boolean isValid(){
         return true;
     }
 
+    public String getGameMode(){
+        String gameMode = sc.nextLine();
+        if (gameMode.equals("1") || gameMode.equals("2") || gameMode.equals("3")) {
+             return gameMode;
+        }
+        else {
+//            display.printWrongValueAlert("1 to 3");
+            getGameMode();
+        }
+        return null;
+    };
+
+    protected String getPlayerName(){
+        String playerName = sc.nextLine();
+        if (playerName.length() > 2){
+            return playerName;
+        }
+        else {
+//            display.printWrongNameAlert();
+            getPlayerName();
+        }
+        return null;
+    }
     public int getBoardSize(){
         System.out.println("Kindly enter the desired size of the board: ");
 
